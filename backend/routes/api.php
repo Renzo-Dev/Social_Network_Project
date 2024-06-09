db881d28102986c8ef555b6171de451c58046cb6
Administrator <dankoshevoy2001@gmail.com>
2024-06-09 22:06:19 +0300

    fixed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-require_once __DIR__ . '/../routes/Authentication/userAuth.php';
+//require_once __DIR__ . '/../routes/Authentication/userAuth.php';
+
+
+Route::get('/info', function () {
+    phpinfo();
+});
