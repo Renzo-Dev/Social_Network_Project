c41439f2494fdf2e39d6696295abc522f62aa4c9
Administrator <dankoshevoy2001@gmail.com>
2024-06-13 15:39:44 +0300

    update login-form add: buttonLogin recoveryPassword

--- frontend/src/components/login-form.vue
+++ frontend/src/components/login-form.vue
@@ -1,13 +1,15 @@
 <template>
   <form class="login-form">
     <h1 class="title-form">Login</h1>
-    <label class="label-container" for="input-email">Email
+    <label class="label-container" for="input-email"><img src="../images/mail.svg" alt="mail">
       <input id="input-email" name="email" type="email" autocapitalize="on" placeholder="Email">
     </label>
-    <label class="label-container" for="input-password">Password
+    <label class="label-container" for="input-password"><img src="../images/lock.svg" alt="">
       <input id="input-password" type="password" autocapitalize="on" placeholder="Password">
     </label>
-    <div class="ErrorMessage">Error Message</div>
+    <div v-show="true" class="ErrorMessage">Error Message</div>
+    <a href="#">{{ $t('messages.recoverPassword') }}</a>
+    <input type="button" id="submit" :value="$t('messages.buttonLogin')" />
   </form>
 </template>
 
@@ -15,6 +17,17 @@
 @import url('../styles/login-form.scss');
 </style>
 
-<script setup>
+<script lang="js">
+import {changeLanguage} from "../i18n";
+
+export default {
+
+  name: 'LoginForm',
+  components: {},
+  methods: {
+    changeLanguage
+  }
+}
+
 
 </script>
