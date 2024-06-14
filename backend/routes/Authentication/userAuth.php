695f11bc95bcd13626dc8d75a08425d1efe2e8db
Administrator <dankoshevoy2001@gmail.com>
2024-06-14 19:00:51 +0300

    fix

--- backend/routes/Authentication/userAuth.php
+++ backend/routes/Authentication/userAuth.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('register', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::get('me', [AuthController::class, 'me']);
-});
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::get('me', [AuthController::class, 'me']);
